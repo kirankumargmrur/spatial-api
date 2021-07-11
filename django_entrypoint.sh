f47148acd22e5142fec3cc16866ff0b95ef1d109
kirankumar-bm-ai <kirankumar@brightmoney.co>
2021-07-11 21:40:04 +0530

    Adding tests and updating docker-compose and entrypoint

--- django_entrypoint.sh
+++ django_entrypoint.sh
@@ -10,12 +10,13 @@ JITTER=
 sleep 45
 #RUN Migrations
 ogr2ogr -f PostgreSQL PG:"host=db user=postgres password=wDnfWovh4uf3 dbname=pixxel" -nlt PROMOTE_TO_MULTI  -lco GEOMETRY_NAME=geometry countries/data/archive/countries.geojson -nln countries -overwrite
-PGPASSWORD=wDnfWovh4uf3  psql -U postgres -d pixxel -h db -c " ALTER TABLE countries RENAME ogc_fid TO id;"
-PGPASSWORD=wDnfWovh4uf3  psql -U postgres -d pixxel -h db -c " ALTER TABLE countries RENAME admin TO name;"
-PGPASSWORD=wDnfWovh4uf3  psql -U postgres -d pixxel -h db -c " ALTER TABLE countries RENAME iso_a3 TO country_code ;"
+PGPASSWORD=wDnfWovh4uf3  psql -U postgres -d pixxel -h db -c "ALTER TABLE countries RENAME ogc_fid TO id;"
+PGPASSWORD=wDnfWovh4uf3  psql -U postgres -d pixxel -h db -c "ALTER TABLE countries RENAME admin TO name;"
+PGPASSWORD=wDnfWovh4uf3  psql -U postgres -d pixxel -h db -c "ALTER TABLE countries RENAME iso_a3 TO country_code ;"
+PGPASSWORD=wDnfWovh4uf3  psql -U postgres -d pixxel -h db -c "ALTER TABLE countries RENAME wkb_geometry TO geometry ;"
 python3 manage.py makemigrations countries
 python3 manage.py migrate
-
+PGPASSWORD=wDnfWovh4uf3  psql -U postgres -d pixxel -h db -c "ALTER TABLE countries ALTER COLUMN geometry type geometry(MultiPolygon, 4326) using ST_Multi(geometry);"
 
 #Start server as gunicorn
 gunicorn pixxel_test.wsgi:application \
